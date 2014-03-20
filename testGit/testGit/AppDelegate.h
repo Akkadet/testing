3b788936621986b3b3453f015493f2d4422ffcf1
Akkadet <akkadet12@hotmail.com>
2014-03-19 21:19:21 -0400

    initial check in

---
+++ testGit/testGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by Oak on 3/19/14.
+//  Copyright (c) 2014 tako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGit/testGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by Oak on 3/19/14.
+//  Copyright (c) 2014 tako. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
